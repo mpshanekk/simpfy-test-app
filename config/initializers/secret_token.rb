a92fca11ce48c3a780203ace4b734a7e9fda05c6
Koketso Mpshane <mpshanekk@gmail.com>
2014-03-13 14:59:32 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CdManagementApp::Application.config.secret_key_base = '5a04cd5ca3b722a876a9803d77586332b98c3124d3158eba8fa3c8838e1377dec9f01be771882eadad3b4f540daec41aa9012278a54f11284fafc347ed3b7f9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CdManagementApp::Application.config.session_store :cookie_store, key: '_cd_management_app_session'
