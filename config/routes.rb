44967d1d373e14cef64a85eff583e98b7f092942
Koketso Mpshane <mpshanekk@gmail.com>
2014-03-17 18:05:35 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ CdManagementApp::Application.routes.draw do
   	 resources :tracks
     end
   end
-  get 'albums' => 'albums#list_albums' 
-
+  #get 'albums' => 'albums#list_albums' 
+  match 'albums', to: 'albums#list_albums', via: 'get'
+  
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}, :path => "auth"
   resources :users
